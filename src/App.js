4bffc6ea8999705d619af7b53bdc7595d071dac1
azim <mdazimmia53@gmail.com>
2024-08-24 18:30:06 +0600

    change data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-library app
+<h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
